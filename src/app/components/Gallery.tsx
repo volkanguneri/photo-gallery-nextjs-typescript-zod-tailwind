aab43f1a95b57ac75990f1c42ee9aa33cb961bfa
root <vocci@hotmail.com>
2024-04-09 16:45:42 +0200

    zod fetch and first display of images

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,20 @@
+import fetchImages from "@/lib/fetchImages";
+import type { ImagesResults } from "@/models/Images";
+import ImgContainer from "./ImgContainer";
+
+export default async function Gallery() {
+  const url = "https://api.pexels.com/v1/curated";
+
+  const images: ImagesResults | undefined = await fetchImages(url);
+
+  if (!images)
+    return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
+
+  return (
+    <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+      {images.photos.map((photo) => (
+        <ImgContainer photo={photo} />
+      ))}
+    </section>
+  );
+}
---
+++ src/app/components/ImgContainer.tsx
@@ -0,0 +1,23 @@
+import type { Photo } from "@/models/Images";
+import Image from "next/image";
+
+type Props = {
+  photo: Photo;
+};
+
+export default function ImgContainer({ photo }: Props) {
+  return (
+    <div
+      key={photo.id}
+      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
+    >
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        sizes="(min-width:1280px) 278px, (max-width:1040px) calc(12.73vw + 118px), (min-width:800px) 33.18vw, (min-width:540px) 50vw, calc(100vw - 16px)"
+        className="object-cover group-hover:opacity-75"
+      />
+    </div>
+  );
+}
