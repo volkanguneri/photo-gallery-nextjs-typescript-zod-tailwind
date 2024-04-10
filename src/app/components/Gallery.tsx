f374fdbf136ee22a51818b294eb4e5119529017a
root <vocci@hotmail.com>
2024-04-10 16:56:46 +0200

    a finished version

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,20 +1,52 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
 import ImgContainer from "./ImgContainer";
+import addBlurredDataUrls from "@/lib/getBase64";
+import getPrevNextPages from "@/lib/getPrevNextPages";
+import Footer from "./Footer";
 
-export default async function Gallery() {
-  const url = "https://api.pexels.com/v1/curated";
+type Props = {
+  topic?: string | undefined;
+  page?: string | undefined;
+};
+
+export default async function Gallery({ topic = "curated", page }: Props) {
+  let url;
+  if (topic === "curated" && page) {
+    // browsing beyond home
+    url = `https://api.pexels.com/v1/curated?page=${page}`;
+  } else if (topic === "curated") {
+    //home
+    url = "https://api.pexels.com/v1/curated";
+  } else if (!page) {
+    // 1st page of search result
+    url = `https://api.pexels.com/v1/search?query=${topic}`;
+  } else {
+    // search result beyond 1st page
+    url = `https://api.pexels.com/v1/search?query=${topic}&
+    page = ${page}`;
+  }
 
   const images: ImagesResults | undefined = await fetchImages(url);
 
-  if (!images)
+  if (!images || images.per_page === 0)
     return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
 
+  const photosWithBlur = await addBlurredDataUrls(images);
+
+  const { prevPage, nextPage } = getPrevNextPages(images);
+
+  const footerProps = { topic, page, nextPage, prevPage };
+
   return (
-    <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
-      {images.photos.map((photo) => (
-        <ImgContainer photo={photo} />
-      ))}
-    </section>
+    <>
+      <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
+        {photosWithBlur.map((photo) => (
+          <ImgContainer key={photo.id} photo={photo} />
+        ))}
+      </section>
+
+      <Footer {...footerProps} />
+    </>
   );
 }
--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,23 +1,40 @@
 import type { Photo } from "@/models/Images";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   photo: Photo;
 };
 
 export default function ImgContainer({ photo }: Props) {
+  const widthHeightRatio = photo.height / photo.width;
+
+  const galleryHeight = Math.ceil(250 * widthHeightRatio);
+  const photoSpans = Math.ceil(galleryHeight / 10) + 1;
+
   return (
     <div
-      key={photo.id}
-      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
+      className="w-[250px] justify-self-center"
+      style={{ gridRow: `span ${photoSpans}` }}
     >
-      <Image
-        src={photo.src.large}
-        alt={photo.alt}
-        fill={true}
-        sizes="(min-width:1280px) 278px, (max-width:1040px) calc(12.73vw + 118px), (min-width:800px) 33.18vw, (min-width:540px) 50vw, calc(100vw - 16px)"
-        className="object-cover group-hover:opacity-75"
-      />
+      <Link
+        href={photo.url}
+        target="_blank"
+        className="grid place-content-center"
+      >
+        <div className="rounded-xl overflow-hidden group">
+          <Image
+            src={photo.src.large}
+            alt={photo.alt}
+            width={250}
+            height={galleryHeight}
+            sizes="250px"
+            placeholder="blur"
+            blurDataURL={photo.blurredDataUrl}
+            className="group-hover:opacity-75"
+          />
+        </div>
+      </Link>
     </div>
   );
 }
